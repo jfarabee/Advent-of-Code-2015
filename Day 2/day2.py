67cefd9d4aa0bf79f1f404c813749e25105b604f
Jake Farabee <45216294+jfarabee@users.noreply.github.com>
2020-02-28 13:48:23 -0700

    Part 2 implementation
    Implemented code to solve pt. 2

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -12,5 +12,10 @@ for input in input_strings:
     nums=input.split('x')
     l, w, h = int(nums[0]), int(nums[1]), int(nums[2])
     surface_area += (2*l*w + 2*w*h + 2*h*l) + min((l*w), (w*h), (h*l))
+    smallest_measures=sorted([l, w, h])[:2]
+    ribbon_length += (2*smallest_measures[0] + 2*smallest_measures[1])
+    ribbon_length += l*w*h
+
 
 print('Total surface area: ', surface_area, ' sqft.')
+print('Total ribbon length: ', ribbon_length, ' ft.')
