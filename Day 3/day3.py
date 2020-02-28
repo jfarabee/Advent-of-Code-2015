bae9da5c0b4021798148241fdd2553423cd2213e
Jake Farabee <45216294+jfarabee@users.noreply.github.com>
2020-02-28 14:33:22 -0700

    Commenting
    some basic explanation of how I did the problem added via comments.

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -10,14 +10,20 @@ if file.mode == "r":
 
 santa_x, santa_y = 0, 0
 
-    #initialize dictionary w/ first house (0,0), which gets one present
+#initialize dictionary w/ first house (0,0), which gets one present
 houses = {
     str(santa_x)+','+str(santa_y) : 1
 }
 
+#dictionary allows for hash-map-adjacent accessing
+#turn int coordinates into a string, use that string as the index, increment
+#value at that coordinate (index) in the houses dictionary
+
 for char in input_string:
     if char == '^':
         santa_y += 1
+        #have to check first if the coordinates have been added to the houses
+        #dictionary. if not, then create the entry and initialize with value 1
         if str(santa_x)+','+str(santa_y) in houses:
             houses[str(santa_x)+','+str(santa_y)] += 1
         else:
@@ -44,7 +50,7 @@ for char in input_string:
 lucky_houses=0
 
 for coord, presents in houses.items():
-    if presents > 1:
+    if presents >= 1:
         lucky_houses += 1
 
 print("Number of lucky houses: ", lucky_houses)
