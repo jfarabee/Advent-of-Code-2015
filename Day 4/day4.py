454996cbd6d839aaf7eb84f62b8d47fa4845002b
Jake Farabee <45216294+jfarabee@users.noreply.github.com>
2020-03-03 11:29:35 -0700

    made day4.py work
    see title

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -4,3 +4,20 @@
 import hashlib
 
 input = "yzbqklnj"
+increment = 1
+encode_string = ""
+sentinel = False
+
+while not sentinel:
+    encode_string = input + str(increment)
+    hash_bytes = hashlib.md5(encode_string.encode())
+    #below: check whether hex (i.e. not byte) representation of MD5 hash has
+    #first 5 characters as '00000'
+    hash_string = hash_bytes.hexdigest()
+    if hash_string[0:5] == "00000":
+        print('Increment for five zeroes is ', increment)
+    #see above for reasoning, part two of day 4 specifies a substring of 6 zeroes
+    if hash_string[0:6] == "000000":
+        print('Increment for six zeroes is ', increment)
+        sentinel = True
+    increment += 1
